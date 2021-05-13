f8f4c50e2428098fe5010b3f0dd8b88c3e4af003
Cody Mangham <cody.w.mangham@gmail.com>
2021-05-13 06:00:11 -0700

    updated sql file

--- schema.sql
+++ schema.sql
@@ -12,8 +12,11 @@ CREATE TABLE users (
 );
 
 CREATE TABLE Friends (
+	chat_id bigint unsigned PRIMARY KEY,
 	user_id1 int not NULL,
     user_id2 int not NULL,
+    confirmed boolean DEFAULT false,
+    foreign key (chat_id) REFERENCES chats(chat_id),
 	FOREIGN KEY (user_id1) REFERENCES users(user_id),
     FOREIGN KEY (user_id2) REFERENCES users(user_id)
 );
@@ -33,20 +36,63 @@ CREATE TABLE group_users (
   FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
+
 CREATE TABLE message (
   message_id bigint unsigned auto_increment PRIMARY KEY,
-  user_id int, -- who sent the message
+  user_id int,
   chat_id bigint unsigned,
   content TEXT,
+  message_type varchar(16) DEFAULT "text",
   time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (chat_id) REFERENCES chats (chat_id),
   FOREIGN KEY (user_id) REFERENCES users (user_id)    
 );  
 
 CREATE TABLE Files(
-	message_id bigint unsigned not null,
+	id int PRIMARY KEY,
+	#message_id bigint unsigned not null,
 	file_name Varchar(100) not null,
-    	file_type varchar(15) not null,
-    	data blob not null,
-    	FOREIGN KEY (message_id) REFERENCES message(message_id)
+    file_type varchar(15) not null,
+    data blob not null,
+    FOREIGN KEY (message_id) REFERENCES message(message_id)
 );
+
+CREATE TABLE Files(
+	id VARCHAR(100) PRIMARY KEY,
+	#message_id bigint unsigned not null,
+	fileName Varchar(100),
+    fileType varchar(100),
+    data blob not null
+    #FOREIGN KEY (message_id) REFERENCES message(message_id)
+);
+
+
+CREATE TABLE notifications (
+  notification_id int Primary KEY,
+  notification_type VARCHAR(10),
+  notification_reciever int,
+  notification_sender int,
+  time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (notification_sender) REFERENCES users (user_id),
+  FOREIGN KEY (notification_reciever) REFERENCES users (user_id)    
+);  
+
+drop trigger create_chat;
+delimiter $$
+CREATE TRIGGER create_chat BEFORE INSERT ON friends
+FOR EACH ROW
+       BEGIN
+           insert into chats set size = 2, chat_name = SUBSTRING(MD5(RAND()) FROM 1 FOR 20);
+		   SET NEW.chat_id = (SELECT MAX(chat_id) from chats);
+       END;$$
+delimiter ;
+
+
+delimiter $$
+CREATE TRIGGER delete_chat_after_friends AFTER DELETE ON friends
+FOR EACH ROW
+       BEGIN
+           DELETE FROM chats WHERE chat_id = old.chat_id;
+           DELETE FROM message WHERE chat_id = old.chat_id;
+       END;$$
+delimiter ;
